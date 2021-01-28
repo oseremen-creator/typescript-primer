de7fffb3427ed62ccf1cd06843970290a5d38f81
Pan-Atlantic <pan-atlantic@Mac-Labs-iMac-Atinuke.local>
2021-01-28 14:57:39 +0100

    my first typescript commit

---
+++ build/index.js
@@ -0,0 +1,12 @@
+const name = `Pius`;
+console.log(name);
+/* let firstname: string = 'Ebuka';
+
+let count: number = 10
+
+firstname = "Paul";
+
+console.log(name)
+
+count = 5 */
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const name: string = `Pius`;
+
+console.log(name)
+
+/* let firstname: string = 'Ebuka';
+
+let count: number = 10
+
+firstname = "Paul";
+
+console.log(name)
+
+count = 5 */
+
